2e972540f6a6652203a392bbfb3b1c1aef39a552
zhangy405 <zhangy405@qq.com>
2016-06-10 19:12:09 +0800

    init

--- bugs_detector/urls.py
+++ bugs_detector/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'bugs_detector0.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^get_count', 'www.views.get_count'),
+    url(r'^.*', 'www.views.home'),
+    # url(r'^admin/', include(admin.site.urls)),
 )
--- www/views.py
+++ www/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+#!coding=utf-8
+import json
+import time
+import os
+import commands
 
+from django.shortcuts import render, render_to_response
+from django.http.response import *
+#from wechat_sdk import WechatConf
+#from wechat_sdk import WechatBasic
+
+#from .sign import Sign
 # Create your views here.
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+exe_path = os.path.join(BASE_DIR, 'bugs.exe')
+
+# conf = WechatConf(
+#         token='your_token',
+#         appid='your_appid',
+#         appsecret='your_appsecret',
+#         encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
+#         encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
+#         )
+#
+# def generate_js_sign(url):
+#     wechat = WechatBasic(conf=conf)
+#     return Sign(wechat.get_jsapi_ticket(), url).sign()
+
+def home(request):
+    return render_to_response('index.html')
+
+def get_count(request):
+    f = request.FILES.get('pic')
+    path = os.path.join(BASE_DIR, 'static')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_name = '%d.jpg' % int(time.time())
+
+    with open(os.path.join(path, file_name), 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+    output = commands.getoutput('%s %s' % (exe_path, path))
+    count = int(output)
+
+    return render_to_response('result.html')
