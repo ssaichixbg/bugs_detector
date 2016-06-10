be39558169220b9397c1fe6962fe49c80f18eabb
zhangy405 <zhangy405@qq.com>
2016-06-10 19:23:02 +0800

    add template setting

--- bugs_detector/settings.py
+++ bugs_detector/settings.py
@@ -82,3 +82,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+TEMPLATES = [
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'APP_DIRS': True,
+  },
+]
