339b36e87ab781899cc5cd877d4fd750d8f3774c
zhangy405 <zhangy405@qq.com>
2016-06-16 09:01:38 +0800

    change UI

--- www/views.py
+++ www/views.py
@@ -7,27 +7,23 @@ from PIL import  Image
 
 from django.shortcuts import render, render_to_response
 from django.http.response import *
-#from wechat_sdk import WechatConf
-#from wechat_sdk import WechatBasic
+from wechat_sdk import WechatConf
+from wechat_sdk import WechatBasic
 
-#from .sign import Sign
+from .sign import Sign
+#from .wxconf import conf
+
 # Create your views here.
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 exe_path = os.path.join(BASE_DIR, 'bugs.exe')
 
-# conf = WechatConf(
-#         token='your_token',
-#         appid='your_appid',
-#         appsecret='your_appsecret',
-#         encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
-#         encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
-#         )
-#
-# def generate_js_sign(url):
-#     wechat = WechatBasic(conf=conf)
-#     return Sign(wechat.get_jsapi_ticket(), url).sign()
+
+def generate_js_sign(url):
+    wechat = WechatBasic(conf=conf)
+    return Sign(wechat.get_jsapi_ticket(), url).sign()
 
 def home(request):
+    #generate_js_sign(request.get_full_path())
     return render_to_response('index.html')
 
 def get_count(request):
